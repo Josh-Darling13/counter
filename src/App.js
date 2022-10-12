b87e236695cea8372002970ded2df0f507140c2b
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-12 10:29:28 -0400

    updated CSS and built react framework files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <form>
         <button class="add_it" type="button">
           Remember this day 1 type stuff?
-          <br></br>Click a button with way too much text in it.
+          <br></br>Click this button with way too much text in it.
           <br></br>
           <small>yes this is a button</small>
         </button>
---
+++ src/CountUp.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+class CountUp{
+    
+const addToCount = () => {
+    
+}
+
+}
+
+
+export default CountUp
