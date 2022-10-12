fe5c101f3b873f35a05fbf669441f31f0bc5154b
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-12 19:45:13 -0400

    basic react works

--- counter/src/Main.js
+++ counter/src/Main.js
@@ -5,13 +5,30 @@ import React, {Component} from 'react';
 class Main extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            num : 0
+          }
+        }
+    increment = () =>{
+        this.setState({num: this.state.num +1})
     }
-    state = {  }
+
+    toZero = () =>{
+        this.setState({num: 0})
+    }
+
+
+
+
     render() {
         return (
-
-                <h1>What's your deal Summer?</h1>
-
+                <div>
+                <h1>It adds numbers and resets</h1>
+                <h1>{this.state.num}</h1>
+                <button className='btn-success' onClick={this.increment}>Inc</button>
+                <button className='btn-danger'>De</button>
+                <button className='btn-primary' onClick={this.toZero} >Nue</button>
+                </div>
           );
     }
 }
