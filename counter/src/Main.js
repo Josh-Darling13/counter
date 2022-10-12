689bf9231f2052ef32d195ecc49167dedb24b76c
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-12 19:14:06 -0400

    added new module Main.app

---
+++ counter/src/Main.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() {
+        return (
+
+                <h1>What's your deal Summer?</h1>
+
+          );
+    }
+}
+
+export default Main;
