40e35ef69ea9a061c17d3159bcd5227ccee122cb
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-12 01:16:57 -0400

    updated css for easier reading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ function App() {
   return (
     <>
       <form>
-        <button class="add_it" type="button">Remember this day 1 type stuff?       <br></br>Click a button with way too much text in it.</button>
+        <button class="add_it" type="button">
+          Remember this day 1 type stuff?
+          <br></br>Click a button with way too much text in it.
+          <br></br>
+          <small>yes this is a button</small>
+        </button>
       </form>
       <br></br>
       <span class="keepgoing">do</span>
