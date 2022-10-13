07b61ff8637b83ecff43b5a1426b313521ee97fd
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-12 20:50:08 -0400

    updated CSS

--- counter/src/Main.js
+++ counter/src/Main.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 
-
-
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -9,6 +7,7 @@ class Main extends Component {
             num : 0
           }
         }
+
     increment = () =>{
         this.setState({num: this.state.num +1})
     }
@@ -17,19 +16,24 @@ class Main extends Component {
         this.setState({num: 0})
     }
 
-
-
-
     render() {
         return (
-                <div>
-                <h1>It adds numbers and resets</h1>
-                <h1>{this.state.num}</h1>
-                    <div className='circular'>
-                        <button className='btn-success' onClick={this.increment}>Add a number</button>
+                <div className='container'>
+                    <div className='header'>
+                        <h1>It adds numbers and resets to zero pretty basic</h1>
+                        <div className='circular'>
+                            <h1> {this.state.num} </h1>
+                        </div>
                     </div>
-                    <div className='circular'>
-                        <button className='btn-primary' onClick={this.toZero}>Reset to zero</button>
+                    <div className='divBod'>
+                        <div className='alignBtn'>
+                            <div className='circular'>
+                                <button className='btn-success' onClick={this.increment}>Add a number</button>
+                            </div>
+                            <div className='circular'>
+                                <button className='btn-primary' onClick={this.toZero}>Reset to zero</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
           );
